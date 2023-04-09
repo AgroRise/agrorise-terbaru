0516f33b0ac8e180d2ff72719d75dc476d681d4c
AgroRise <agrorise2023@gmail.com>
2023-04-09 21:55:10 +0700

    menambahkan projek baru

---
+++ resources/views/login.blade.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Login - AgroRise</title>
+
+        <!-- CSS FILES -->        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/login.css" rel="stylesheet">
+<!--
+
+
+
+-->
+    </head>
+    
+    <body>
+
+        <main>
+        <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="index.html">
+                        <i class="navbar-brand-icon bi-book me-2"></i>
+                        <span>AgroRise</span>
+                    </a>
+
+                    <div class="d-lg-none ms-auto me-3">
+                        <a href="#" class="btn custom-btn custom-border-btn btn-naira btn-inverted">
+                            <i class="btn-icon bi-cloud-download"></i>
+                            <span>Masuk</span><!-- duplicated another one below for mobile -->
+                        </a>
+                    </div>
+    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-lg-auto me-lg-4">
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_1">Beranda</a>
+                            </li>
+    
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_2">Course</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Perhitungan</a>
+
+                                <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="">Pupuk</a></li>
+                                    <li><a class="dropdown-item" href="">Bibit</a></li>
+                                    <li><a class="dropdown-item" href="">Keuntungan</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_5">Tentang Kami</a>
+                            </li>
+                        </ul>
+
+                        <div class="d-none d-lg-block">
+                            <a href="#" class="btn custom-btn custom-border-btn btn-naira btn-inverted">
+                                <i class="btn-icon bi-cloud-download"></i>
+                                <span>Masuk</span><!-- duplicated above one for mobile -->
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="bgimg">
+                <div class="wrapper fadeInDown">
+                    <div id="formContent">
+                    <!-- Tabs Titles -->
+                    <h2 class="active"> Sign In </h2>
+                    
+                
+                    <!-- Icon -->
+                    <div class="fadeIn first">
+                        <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                    </div>
+                
+                    <!-- Login Form -->
+                    <form>
+                        <input type="text" id="login" class="fadeIn second" name="login" placeholder="Email">
+                        <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password">
+                        <input type="submit" class="fadeIn fourth" value="Log In">
+                    </form>
+                
+                    <!-- Remind Passowrd -->
+                    <div id="formFooter">
+                        <a class="underlineHover" href="#">Lupa Password?</a> <p>atau</p>
+                        <a class="underlineHover" href="#">Mendaftar</a>
+                    </div>
+                
+                    </div>
+                </div>
+            </div>
+
+
+
+            
+        </main>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/click-scroll.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
